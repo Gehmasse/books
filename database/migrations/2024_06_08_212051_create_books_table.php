708bb830a7733a3252fe8aa920adb3c9f0792f7b
Leo Nickl <leo@leonickl.de>
2024-06-08 23:26:19 +0200

    books migration

---
+++ database/migrations/2024_06_08_212051_create_books_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->enum('status', ['toread', 'reading', 'finished', 'aborted']);
+            $table->string('author');
+            $table->date('started_at');
+            $table->date('finished_at')->nullable();
+            $table->string('info');
+            $table->unsignedTinyInteger('rating')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
